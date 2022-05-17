154de0955c47d943e453d54ee8862873e9af1bf3
aromero7764 <aromero7764@gmail.com>
2022-05-17 15:59:59 -0400

    Primer commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("template/cabecera.php") ?>
+
+
+
+            <div class="jumbotron">
+                <h1 class="display-3">Bienvenidos al mundo de los libros</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="productos.php" role="button">Jumbo action name</a>
+                </p>
+            </div>
+
+            <?php include("template/pie.php") ?>
---
+++ template/cabecera.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Develoteca</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="productos.php">Libros</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="nosotros.php">Nosotros</a>
+            </li>
+            
+
+        </ul>
+    </nav>
+
+    <div class="container">
+</br>
+        <div class="row">
